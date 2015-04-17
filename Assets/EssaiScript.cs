f65582ffc00f0635b4942f2dc278355d2f90433b
Fernando VELEZ <velez@vd-dev.com>
2015-04-17 11:35:59 +0200

    First checkin
    just another git projet

---
+++ Assets/EssaiScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EssaiScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
